1398d5e99f02fe12bdd1a413aa68203b62fcd959
Vaibhav <vaibhavovhal15@gmail.com>
2023-04-27 20:50:07 +0530

    [Vaibhav] Add:Welcome message

---
+++ LineComparisonProblem/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LineComparisonProblem
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Line Comparison Program.");
+        }
+    }
+}
