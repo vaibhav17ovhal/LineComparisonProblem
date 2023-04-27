4ac24733acf8615ff6831bbb5e0a218ecaaf33ec
vaibhav17ovhal <125484163+vaibhav17ovhal@users.noreply.github.com>
2023-04-27 21:15:43 +0530

    Update Operation.cs

--- LineComparisonProblem/Operation.cs
+++ LineComparisonProblem/Operation.cs
@@ -8,25 +8,6 @@ namespace LineComparisonProblem
 {
     public class Operation
     {
-        public static void LengthOfLine()
-        {
-            Console.Write("Enter the value of x1: ");
-            int x1 = Convert.ToInt32(Console.ReadLine());
-
-            Console.Write("Enter the value of y1: ");
-            int y1 = Convert.ToInt32(Console.ReadLine());
-
-            Console.Write("Enter the value of x2: ");
-            int x2 = Convert.ToInt32(Console.ReadLine());
-
-            Console.Write("Enter the value of y2: ");
-            int y2 = Convert.ToInt32(Console.ReadLine());
-
-            //Length of Line = sqrt[(x2-x1)^2 + (y2-y1)^2]
-            double result = Math.Sqrt(Math.Pow((x2-x1), 2) + Math.Pow((y2-y1), 2));
-            Console.WriteLine(result);
-
-        }
         public static void EqualityCheck(double line1 , double line2)
         {
             if (line1.Equals(line2))
