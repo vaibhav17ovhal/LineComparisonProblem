cb1a72d4d0d4fabe0ef795c2ab8779a54b3433ec
vaibhav17ovhal <125484163+vaibhav17ovhal@users.noreply.github.com>
2023-05-05 16:08:15 +0530

    Update Program.cs

--- LineComparisonProblem/Program.cs
+++ LineComparisonProblem/Program.cs
@@ -6,22 +6,6 @@ namespace LineComparisonProblem
     {
         static void Main(string[] args)
         {
-            /*
-            Console.WriteLine("Welcome to Line Comparison Program.");
-
-            Console.WriteLine("==============X=============");
-
-            Operation.LengthOfLine();
-
-            Console.WriteLine("==============X=============");
-
-            Operation.EqualityCheck(15 , 15.5);
-
-            Console.WriteLine("==============X=============");
-
-            Operation.Comparison(15, 15.5);
-            */
-
             Compare compare = new Compare();
             compare.Check();
         }
